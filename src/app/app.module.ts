d432e3faacaf7250d615b68e24a2476878d6f038
Queemel <emanuelquesta@gmail.com>
2023-10-12 10:33:45 -0300

    Changes to be committed: 	modified:   src/app/app.module.ts 	new file:   src/app/pista-juego/pista-juego.component.css 	new file:   src/app/pista-juego/pista-juego.component.html 	new file:   src/app/pista-juego/pista-juego.component.ts 	modified:   tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PistaJuegoComponent } from './pista-juego/pista-juego.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PistaJuegoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pista-juego/pista-juego.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pista-juego',
+  templateUrl: './pista-juego.component.html',
+  styleUrls: ['./pista-juego.component.css']
+})
+export class PistaJuegoComponent {
+
+}
