c13b0c038a2745b615e52008cafe21fe5b7c9d06
[JoaquinM22] <[albarracin.joaquin22@gmail.com]>
2023-10-12 14:31:09 -0300

    Creacion de Modulo mostrarFoto, donde mostrara Las fotos de la API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PistaJuegoComponent } from './pista-juego/pista-juego.component';
+import { MostrarFotoComponent } from './mostrar-foto/mostrar-foto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PistaJuegoComponent
+    PistaJuegoComponent,
+    MostrarFotoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mostrar-foto/mostrar-foto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-foto',
+  templateUrl: './mostrar-foto.component.html',
+  styleUrls: ['./mostrar-foto.component.css']
+})
+export class MostrarFotoComponent {
+
+}
