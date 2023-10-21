b75fa8ace8186f2e46f328d9e612fd5e83a49e23
FrancoGarcia <francogarciauni@hotmail.com>
2023-10-21 16:27:25 -0300

    Agregado fondo/Creacion de base ts para login  On branch Franco  Changes to be committed: 	new file:   src/12804207_5073414.jpg 	deleted:    src/app/Mass_Effect_2.jpg 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/login-usuario/login-usuario.component.html 	modified:   src/app/login-usuario/login-usuario.component.ts 	modified:   src/app/menu-inicio/menu-inicio.component.css 	modified:   src/styles.css

--- src/app/login-usuario/login-usuario.component.ts
+++ src/app/login-usuario/login-usuario.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login-usuario',
   templateUrl: './login-usuario.component.html',
   styleUrls: ['./login-usuario.component.css']
 })
-export class LoginUsuarioComponent {
+
+export class LoginUsuarioComponent implements OnInit{
+  
+  constructor()
+  {
+
+  }
+
+  usuarioNuevo: any;
+  //Este segundo seria el que traigo de la base de datos para compararlo
+  //usuarioRegistrado: any
+
+  //Funcion de guardar los datos de usuario
+  logueo_Usuario()
+  {
+    class usuario
+    {
+      nombreUsuario: String;
+      password: String;
+
+      constructor(nombreUsuario: String, password: String)
+      {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+      }
+    }
+
+    const botonInicioSesion = document.querySelector("#ingresar");
+
+    if(botonInicioSesion)
+    {
+      botonInicioSesion.addEventListener("click", (evento) => 
+      {
+        evento.preventDefault();
+
+        const nombre = (<HTMLInputElement>document.getElementById("Usuario")).value;
+        const password = (<HTMLInputElement>document.getElementById("Password")).value;
+
+        this.usuarioNuevo = new usuario(nombre, password);
+
+        //Falta agregar la funcion que lo compare con los registros
+
+      });
+    }
+  }
+
+  ngOnInit()
+  {
+    this.logueo_Usuario();
+  }
 
 }
