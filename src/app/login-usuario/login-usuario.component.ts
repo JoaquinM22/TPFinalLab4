ea30a4b6c8f460bffeb4dfa67c3a75e1e83beea1
FrancoGarcia <francogarciauni@hotmail.com>
2023-10-24 18:17:31 -0300

    Creacion de base ts para login  On branch Franco  Changes to be committed: 	modified:   src/app/backend/db.json 	modified:   src/app/login-usuario/login-usuario.component.css 	modified:   src/app/login-usuario/login-usuario.component.html 	modified:   src/app/login-usuario/login-usuario.component.ts

--- src/app/login-usuario/login-usuario.component.ts
+++ src/app/login-usuario/login-usuario.component.ts
@@ -14,11 +14,12 @@ export class LoginUsuarioComponent implements OnInit{
   }
 
   usuarioLogueado: any;
+  usuarioaValidar: any;
   usuarioCreado: any;
-  registroUsuario: any;
+  
 
   //Funcion de guardar los datos de usuario
-  logueo_Usuario()
+  async logueo_Usuario()
   {
     class usuario
     {
@@ -36,26 +37,36 @@ export class LoginUsuarioComponent implements OnInit{
 
     if(botonInicioSesion)
     {
-      botonInicioSesion.addEventListener("click", (evento) => 
+      botonInicioSesion.addEventListener("click", async (evento) => 
       {
         evento.preventDefault();
 
         const nombre = (<HTMLInputElement>document.getElementById("Usuario")).value;
         const password = (<HTMLInputElement>document.getElementById("Password")).value;
 
-        this.usuarioLogueado = new usuario(nombre, password);
+        this.usuarioaValidar = new usuario(nombre, password);
 
         //Falta agregar la funcion que lo compare con los registros
 
-        this.validarUsuario(this.usuarioLogueado);
+        const validado = await this.validarUsuario(nombre, password);
 
-        console.log(this.registroUsuario);
+        if(validado)
+        {
+          console.log(this.usuarioLogueado);
+        }else
+        {
+          var mensaje = document.getElementById("txt_login");
+          if(mensaje)
+          {
+            mensaje.innerHTML = "Password incorrecto"
+          }
+        }
 
       });
     }
   }
 
-  /* No entra al if de comparacion de contraseña
+
   creacion_Usuario()
   {
     class usuarioCre
@@ -82,64 +93,71 @@ export class LoginUsuarioComponent implements OnInit{
         const passwordNew = (<HTMLInputElement>document.getElementById("PasswordNuevo")).value;
         const confPassword = (<HTMLInputElement>document.getElementById("PasswordConfirm")).value;
 
-        if(passwordNew != confPassword)
+        //Confirmacion de password
+        if(passwordNew !== confPassword)
         {
-          var mensaje = document.getElementById("#texto");
+          var mensaje = document.getElementById("texto");
           if(mensaje)
           {
             mensaje.innerHTML = "Los passwords no coinciden";
           }
+        }else if(passwordNew === confPassword)
+        {
+          //Crea el usuario y lo manda a la funcion que lo carga en el server
+          this.usuarioCreado = new usuarioCre(nombreNew, passwordNew);
+          //Aca va la funcion que lo carga en la "BDD"
         }else
         {
-          this.usuarioCreado = new usuarioCre(nombreNew, passwordNew);
-          console.log(this.usuarioCreado);
+          var mensaje = document.getElementById("texto");
+          if(mensaje)
+          {
+            mensaje.innerHTML = "ERROR";
+          }
         }
-        
-
-        //Falta agregar la funcion que lo agregue a los registros
 
       });
     }
-  }*/
+  }
 
-  validarUsuario(usuario: any)
-  {
-    class registrado
-    {
-      nombreUsuario: String;
-      password: String;
-
-      constructor(nombreUsuario: String, password: String)
-      {
+  async validarUsuario(nombre: string, contra: string) {
+    var validacion: boolean = false;
+  
+    class registrado {
+      nombreUsuario: string;
+      password: string;
+  
+      constructor(nombreUsuario: string, password: string) {
         this.nombreUsuario = nombreUsuario;
         this.password = password;
       }
     }
-
-    const servidor = "http://localhost:3000/users"
-
-    fetch(servidor)
-    .then(res => res.json())
-    .then(data =>
-    {
-      console.log("Respuesta de servidor: ", data);
-
-      //Hay un error donde no deja usar el for
-      for(const datos of data.results)
-      {
-        if(datos.usuario == usuario.nombre)
-        {
-          this.registroUsuario = new registrado(datos.usuario, datos.password);
+  
+    const servidor = "http://localhost:3000/users";
+  
+    try {
+      const response = await fetch(servidor);
+      const json = await response.json();
+  
+      for (const datos of json) {
+        if (datos.usuario === nombre) {
+          if (datos.password === contra) {
+            this.usuarioLogueado = new registrado(datos.usuario, datos.password);
+            validacion = true;
+          }
         }
       }
-    })
-
+    } catch (error) {
+      console.error("Error al obtener datos del servidor", error);
+    }
+  
+    return validacion;
   }
+  
 
   ngOnInit()
   {
     this.logueo_Usuario();
-    //this.creacion_Usuario();
+    this.creacion_Usuario();
   }
 
 }
