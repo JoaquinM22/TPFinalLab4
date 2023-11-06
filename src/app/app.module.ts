69ad118719bd2250c19cc6488ed29cc40795b731
Queemel <emanuelquesta@gmail.com>
2023-11-06 09:10:39 -0300

    On branch Emanuel  Your branch is ahead of 'PP4/Emanuel' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   src/app/app.module.ts
    	renamed:    src/app/components/login-usuario/login-usuario.component.css -> src/app/components/pantallas/login-usuario/login-usuario.component.css
    	renamed:    src/app/components/login-usuario/login-usuario.component.html -> src/app/components/pantallas/login-usuario/login-usuario.component.html
    	renamed:    src/app/components/login-usuario/login-usuario.component.ts -> src/app/components/pantallas/login-usuario/login-usuario.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { PistaJuegoComponent } from './components/game-components/pista-juego/pista-juego.component';
 import { MostrarFotoComponent } from './components/game-components/mostrar-foto/mostrar-foto.component';
 import { TemporizadorComponent } from './components/game-components/temporizador/temporizador.component';
-import { LoginUsuarioComponent } from './components/login-usuario/login-usuario.component';
+import { LoginUsuarioComponent } from './components/pantallas/login-usuario/login-usuario.component';
 import { MenuInicioComponent } from './components/menu-inicio/menu-inicio.component';
 import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
 import { RankingComponent } from './ranking/ranking.component';
